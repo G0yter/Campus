35b9dc7f6434bf75a8746dd43fa21f19fddff707
G0yter <goyter012@gmail.com>
2020-06-17 23:43:00 +0300

    initial UI created + StudentController added

---
+++ src/main/java/com/gmail/goyter012/campus/controller/StudentController.java
@@ -0,0 +1,40 @@
+package com.gmail.goyter012.campus.controller;
+
+import com.gmail.goyter012.campus.service.StudentService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+public class StudentController {
+
+    private StudentService studentService;
+
+    @Autowired
+    public void setStudentService(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping("/")
+    public String start(){
+        return "index";
+    }
+
+
+    @GetMapping("/main")
+    public String studentsList(Model model) {
+        model.addAttribute("students", studentService.allStudents());
+        return "stud/students";
+    }
+
+
+
+
+
+
+}
---
+++ src/main/resources/templates/stud/students.ftl
@@ -0,0 +1,6 @@
+<#import "../parts/common.ftl" as c>
+
+
+<@c.page>
+        STUDENTS PAGE WILL BE HERE HERE
+</@c.page>
