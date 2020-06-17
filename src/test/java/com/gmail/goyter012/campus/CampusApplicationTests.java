ba9449acb6933bb4bd1c83e3b00b7942bace3d8f
G0yter <goyter012@gmail.com>
2020-06-17 20:42:27 +0300

    initial commit

---
+++ src/test/java/com/gmail/goyter012/campus/CampusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.goyter012.campus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
