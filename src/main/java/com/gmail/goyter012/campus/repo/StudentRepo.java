6d83fee67e353636a11a476fe7f1e539e520680a
G0yter <goyter012@gmail.com>
2020-06-19 06:36:12 +0300

    student repo modified

--- src/main/java/com/gmail/goyter012/campus/repo/StudentRepo.java
+++ src/main/java/com/gmail/goyter012/campus/repo/StudentRepo.java
@@ -1,10 +1,18 @@
 package com.gmail.goyter012.campus.repo;
 
+import com.gmail.goyter012.campus.model.Faculty;
 import com.gmail.goyter012.campus.model.Student;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StudentRepo extends CrudRepository<Student, Long> {
 
+    List<Student> findAllByFacultyEquals(Faculty faculty);
+
+    List<Student> findAllByCardNumber(Integer cardNumber);
+
+    List<Student> findAllByFullName(String fullName);
 }
