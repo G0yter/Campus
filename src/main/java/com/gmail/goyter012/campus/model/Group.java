6b176c8197f7d27df2926ea2fa77b9d870f06f62
semerey <fyzctvthtq@gmail.com>
2020-06-19 06:43:40 +0300

    group model created

---
+++ src/main/java/com/gmail/goyter012/campus/model/Group.java
@@ -0,0 +1,30 @@
+package com.gmail.goyter012.campus.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "stud_gr")
+@Data
+public class Group {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "cipher_gr")
+    private String cipher;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "faculty_id")
+    private Faculty faculty;
+
+    @OneToMany(mappedBy = "groupOfStuds", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Student> students = new ArrayList<>();
+
+
+}
+
