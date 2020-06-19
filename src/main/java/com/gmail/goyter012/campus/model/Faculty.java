9ef958d00f21352e2452725a4ac4e41bb3b5fea8
Champerson <Violentoleg@gmail.com>
2020-06-19 07:00:33 +0300

    faculty model created

---
+++ src/main/java/com/gmail/goyter012/campus/model/Faculty.java
@@ -0,0 +1,34 @@
+package com.gmail.goyter012.campus.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "faculty")
+@Data
+public class Faculty {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "faculty_name")
+    private String name;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "dec_full_name")
+    private String decanName;
+
+    @OneToMany(mappedBy = "faculty", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Group> groups = new ArrayList<>();
+
+    @OneToMany(mappedBy = "faculty", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Student> students = new ArrayList<>();
+
+
+}
